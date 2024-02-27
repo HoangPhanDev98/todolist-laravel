d3cae4404eaecf005f934abd1fdfc817fb6d9f44
Phan Thanh Hoàng <hoangphandev98@gmail.com>
2024-02-27 22:03:36 +0700

    Styling form, flash message and add Alpine.js

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -2,57 +2,56 @@
 
 @section('title', isset($task) ? 'Edit Task' : 'Add Task')
 
-@section('styles')
-    <style>
-        .error-message {
-            color: red;
-            font-size: 0,8 rem;
-        }
-    </style>
-@endsection
-
 @section('content')
     <form method="POST" action="{{ isset($task) ? route('tasks.update', ['task' => $task->id]) : route('tasks.store') }}">
         @csrf
         @isset($task)
             @method('PUT')
         @endisset
-        <div>
+        <div class="mb-4">
             <label for="title">Title</label>
-            <input type="text" name="title" id="title" value="{{ $task->title ?? old('title') }}"/>
+            <input type="text" name="title" id="title" value="{{ $task->title ?? old('title') }}"
+                @class(['border-red-500' => $errors->has('title')])
+            />
             @error('title')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
 
-        <div>
+        <div class="mb-4">
             <label for="description">Description</label>
-            <textarea type="text" name="description" id="description" rows="5">
+            <textarea type="text" name="description" id="description" rows="5"
+                @class(['border-red-500' => $errors->has('description')])
+            >
                 {{ $task->description ?? old('description') }}
             </textarea>
             @error('description')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
 
-        <div>
+        <div class="mb-4">
             <label for="long_description">Long Description</label>
-            <textarea type="text" name="long_description" id="long_description" rows="10">
+            <textarea type="text" name="long_description" id="long_description" rows="10"
+                @class(['border-red-500' => $errors->has('long_description')])
+            >
                 {{ $task->long_description ?? old('long_description') }}
             </textarea>
             @error('long_description')
-                <p class="error-message">{{ $message }}</p>
+                <p class="error">{{ $message }}</p>
             @enderror
         </div>
 
-        <div>
-            <button type="submit">
+        <div class="flex gap-2">
+            <button type="submit" class="btn">
                 @isset($task) 
                     Update Task
                 @else
                     Add Task
                 @endisset
             </button>
+
+            <a href="{{ route('tasks.index') }}" class="btn">Cancel</a>
         </div>
     </form>
 @endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,44 +5,48 @@
 @section('content')
 
 <div class="mb-4">
-<a href="{{ route('tasks.index') }}" class="font-medium text-gray-700 underline decoration-pink-500">⬅️ Go back to the task list!</a>
+    <a href="{{ route('tasks.index') }}" class="link">⬅️ Go back to the task list!</a>
 </div>
 
 <p class="mb-4 text-slate-700">{{ $task->description }}</p>
 
 @isset($task->long_description)
-    <p>{{ $task->long_description }}</p>
+    <p class="mb-4 text-slate-700">{{ $task->long_description }}</p>
 @endisset
 
-<p>{{ $task->created_at }}</p>
+<p class="mb-4 text-sm text-slate-500">
+    Created {{ $task->created_at->diffForHumans() }} -
+    Updated {{ $task->updated_at->diffForHumans() }}
+</p>
 
-<p>{{ $task->updated_at }}</p>
-
-<p>
-    @if ($task->completed)
-    Completed
+<p class="mb-4">
+    @if($task->completed)
+        <span class="font-medium text-green-500">Completed</span>
     @else
-    Not Completed
+        <span class="font-medium text-red-500">Not Completed</span>
     @endif
 </p>
 
-<a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
 
-<div>
-    <form method="POST" action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
+<div class="flex gap-2">
+    <a href="{{ route('tasks.edit', ['task' => $task]) }}"
+        class="btn">Edit</a>
+
+    <form method="POST"
+        action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
         @csrf
         @method('PUT')
-        <button type="submit">
-            Mark as {{ $task->completed ? 'not completed' : 'completed' }}
+        <button type="submit" class="btn">
+            Mark as
+            {{ $task->completed ? 'not completed' : 'completed' }}
         </button>
     </form>
-</div>
 
-<div>
-    <form method="POST" action="{{ route('tasks.destroy', ['task' => $task->id]) }}">
+    <form method="POST"
+        action="{{ route('tasks.destroy', ['task' => $task->id]) }}">
         @csrf
         @method('DELETE')
-        <button type="submit">Delete</button>
+        <button type="submit" class="btn">Delete</button>
     </form>
 </div>
 @endsection
